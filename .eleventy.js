149096e36b4910774589f75a79bc4ed797aeef11
HangerThem <f.borisjuk@hangerthem.com>
2025-08-20 14:14:29 +0200

    Added events collection to Eleventy config, updated public folder path in admin config, and added new events to the homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,23 @@
 const { DateTime } = require("luxon")
+const fs = require("fs")
+const path = require("path")
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("formatDate", (dateString, format) => {
     let dt = DateTime.fromISO(dateString)
     if (!dt.isValid) dt = DateTime.local()
     return dt.setLocale("cs").toFormat(format)
+  })
+  eleventyConfig.addCollection("events", () => {
+    const dir = "src/_data/events"
+    return fs
+      .readdirSync(dir)
+      .filter((file) => file.endsWith(".json"))
+      .map((file) => {
+        const filepath = path.join(dir, file)
+        const data = JSON.parse(fs.readFileSync(filepath))
+        return { file, ...data }
+      })
   })
 
   eleventyConfig.addPassthroughCopy("src/assets")
