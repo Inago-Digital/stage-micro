9b8b2fbe02e2c9e12b92278ed97befcf3e5477e9
HangerThem <f.borisjuk@hangerthem.com>
2026-03-08 11:08:13 +0100

    Events sorting and filtering based on date

--- frontend/app/(homepage)/layout.tsx
+++ frontend/app/(homepage)/layout.tsx
@@ -5,5 +5,13 @@ import { Event } from "@/types/Event"
 export default async function Layout() {
   const events = await fetchStrapi<Event[]>("events")
 
+  events
+    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+    .filter((event) => {
+      const eventDate = new Date(event.date)
+      const now = new Date()
+      return eventDate >= now
+    })
+
   return <Home events={events} />
 }
